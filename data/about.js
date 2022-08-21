56aedf0e22d62671c8310dcc4cdce7d0432c8a25
Kyle Canamar <canamar.kyle@gmail.com>
2022-08-20 17:22:04 -0700

    creates a reusable directory of various reusable data for faster updates.

---
+++ data/about.js
@@ -0,0 +1,9 @@
+export default {
+    name: "Funcy Koder",
+    title: "Funcy Koder's Blog",
+    headshot: "/public/images/headshot.png",
+    github: "https://github.com/kcanamar",
+    linkedin: "https://www.linkedin.com/in/kyle-canamar/",
+    twitter: "https://twitter.com/funcy_koder",
+    portfolio: "https://kylecanamar-portfolio.vercel.app/",
+}
---
+++ data/theme.js
@@ -0,0 +1,8 @@
+export default {
+    primary: "#2ec4b6",
+    primaryVar: "#011627",
+    secondary: "#ff9f1c",
+    secondaryVar: "#e71d36",
+    bg: "#fdfffc",
+    pageWidth: "80%",
+}
